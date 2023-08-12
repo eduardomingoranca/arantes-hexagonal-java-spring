1ffb62a8db80d91fa93e64cba5a7a9dc2779d0ec
eduardomingoranca <eduardomingoranca@gmail.com>
2023-08-12 11:04:42 -0300

    criando o adapter

---
+++ arantes/src/main/java/com/brazil/arantes/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,23 @@
+package com.brazil.arantes.adapters.out;
+
+import com.brazil.arantes.adapters.out.client.mapper.CustomerEntityMapper;
+import com.brazil.arantes.adapters.out.repository.CustomerRepository;
+import com.brazil.arantes.adapters.out.repository.entity.CustomerEntity;
+import com.brazil.arantes.application.core.domain.Customer;
+import com.brazil.arantes.application.ports.out.UpdateCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        CustomerEntity customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+
+}
