c6279ac56f72e5175552d44bfb02908e67c5c740
eduardomingoranca <eduardomingoranca@gmail.com>
2023-08-12 10:24:51 -0300

    criando o client

--- arantes/src/main/java/com/brazil/arantes/ArantesApplication.java
+++ arantes/src/main/java/com/brazil/arantes/ArantesApplication.java
@@ -2,7 +2,9 @@ package com.brazil.arantes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // habilitando o uso do feign client
 @SpringBootApplication
 public class ArantesApplication {
 
