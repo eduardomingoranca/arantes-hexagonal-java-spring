693ef240bf5f93c5b8a10921caa53a8bc0b6bcd8
eduardomingoranca <eduardomingoranca@gmail.com>
2023-08-12 10:14:33 -0300

    criando o projeto

---
+++ arantes/src/main/java/com/brazil/arantes/ArantesApplication.java
@@ -0,0 +1,13 @@
+package com.brazil.arantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArantesApplication.class, args);
+	}
+
+}
